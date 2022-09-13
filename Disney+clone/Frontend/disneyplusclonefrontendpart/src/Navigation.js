284043ec61d12827d259110112a40ae3f5ffb6e2
unknown <acsgyorgy.mail@gmail.com>
2022-09-13 15:34:20 +0200

    navigation router added

---
+++ Disney+clone/Frontend/disneyplusclonefrontendpart/src/Navigation.js
@@ -0,0 +1,24 @@
+import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from "./Pages/Home";
+import Search from "./Pages/Search";
+import MyList from "./Pages/MyList";
+import Movies from "./Pages/Movies";
+import Series from "./Pages/Series";
+
+
+const Navigation = () => {
+  return (
+    <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="search" element={<Search />} />
+          <Route path="mylist" element={<MyList />} />
+          <Route path="movies" element={<Movies />} />
+          <Route path="series" element={<Series />} />
+        </Routes>
+    </Router>
+  );
+}
+
+export default Navigation;
