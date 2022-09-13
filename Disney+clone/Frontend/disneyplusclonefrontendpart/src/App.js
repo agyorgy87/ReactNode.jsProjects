d2feadee925f6442dfdcaf86d19f39d291b0e3d4
unknown <acsgyorgy.mail@gmail.com>
2022-09-13 12:34:47 +0200

    the data arrived from the backend

--- Disney+clone/Frontend/disneyplusclonefrontendpart/src/App.js
+++ Disney+clone/Frontend/disneyplusclonefrontendpart/src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    const [allMovies, setAllMovies] = useState([]);
+    const [movieTitle, setMovieTitle] = useState("");
+
+    useEffect(() => {
+        fetch("http://localhost:4000/all-movies")
+        .then(data => data.json())
+        .then(parsedData => setAllMovies(parsedData))
+    }, [])
+
+    return (
+        <div className="App">
+            {allMovies.length === 0 ? "Loading..." : null}
+
+            {
+                allMovies.map( movies => (
+                    <div>
+                       <span> {movies.movieMainTitle} </span> - <span> {movies.type} </span>
+                       <img src={"http://localhost:4000/icons/" + movies.icon} style={{width: "200px"}} alt="moviepicture"/>
+                       <img src={"http://localhost:4000/img/" + movies.image} style={{width: "200px"}} alt="moviepicture"/>
+                    </div>
+                ))
+            }
+        </div>
   );
 }
 
