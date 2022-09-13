d534b9e51ae1dfc4ab0d633745eea440e224c4e9
unknown <acsgyorgy.mail@gmail.com>
2022-09-13 12:40:12 +0200

    json file modified, adding pictures data.

--- Disney+clone/Backend/server.js
+++ Disney+clone/Backend/server.js
@@ -17,6 +17,29 @@ app.get("/", (request,response) => {
     response.end(JSON.stringify(responseObject));
 });
 
+//API to access json data
+app.get("/all-movies", (request, response) => {
+    fileSystem.readFile("allMoviesData.json", "utf8", (error, data) => {
+        if(error){
+            let errorResponse = { error: "A fájl olvasása közben hiba történt."}
+            response.end(JSON.stringify(errorResponse));
+        } else {
+            response.end(data);
+        }
+    })
+})
+
+//API for icons
+app.get("/icons/:filename", function (req, res) {
+    res.sendFile(path.join(__dirname, "icons/" + req.params.filename));
+})
+
+//API for images
+app.get("/img/:filename", function (req, res) {
+    res.sendFile(path.join(__dirname, "img/" + req.params.filename));
+})
+
+
 const server = app.listen(app.get("port"), function() {
 
     let host = server.address().address
